5f3ae60a96dfa6cbdc2b42d7945a3e91b801a30c
ThickGuy03 <adamgoral@onet.eu>
2023-10-18 13:35:08 +0200

    Login Form
    utworzenie okienka logowania, sczytywanie danych z databasa, wyłapywanie błędnych danych

---
+++ MójE-Bank/Program.cpp
@@ -0,0 +1,22 @@
+﻿#include "LoginForm.h"
+
+using namespace System;
+using namespace System::Windows::Forms;
+
+void main(array<String^>^ args)
+{
+	Application::EnableVisualStyles();
+	Application::SetCompatibleTextRenderingDefault(false);
+	MójEBank::LoginForm loginForm;
+
+	loginForm.ShowDialog();
+	User^ user = loginForm.user;
+
+	if (user != nullptr)
+	{
+		MessageBox::Show("Udana weryfikacja " + user->name+" "+user->surname, "Program.cpp", MessageBoxButtons::OK);
+	}
+	else {
+		MessageBox::Show("Weryfikacja nieudana", "Program.cpp", MessageBoxButtons::OK);
+	}
+}
